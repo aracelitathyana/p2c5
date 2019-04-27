58280eb4600f23f4a6fd1ea76b61ceda905c98b9
Araceli Tathyana <aracelitathyana@gmail.com>
2019-04-27 08:11:21 -0500

    modificas el nombre de la bd

---
+++ conexion.php
@@ -0,0 +1,18 @@
+<?php
+$driver = "mysql";
+$base = "bd_p2c5";
+$host = "localhost";
+$usuario = 'root';
+$contraseña = '';
+
+
+$dsn = "$driver:dbname=$base;host=$host";
+
+try {
+    $cnx = new PDO($dsn, $usuario, $contraseña);
+    echo 'hola ara' ;
+} catch (PDOException $e) {
+    echo 'Falló la conexión: ' . $e->getMessage();
+}
+
+?>
